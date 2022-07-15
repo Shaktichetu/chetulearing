fe3b51bb3d0ea6f57f196e159d381ae36a2cf369
Shakti Tripathi <shaktit@chetu.com>
2022-07-15 22:36:54 +0530

    update

--- camera_demo/lib/main.dart
+++ camera_demo/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
--- camera_demo/lib/screens/add_place_screen.dart
+++ camera_demo/lib/screens/add_place_screen.dart
@@ -26,6 +26,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
     if (_titleController.text.isEmpty || _pickedImage == null) {
       return;
     }
+    
     Provider.of<GreatPlaces>(context, listen: false)
         .addPlace(_titleController.text, _pickedImage!);
     Navigator.of(context).pop();
