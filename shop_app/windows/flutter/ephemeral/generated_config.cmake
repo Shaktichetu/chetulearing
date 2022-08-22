1a257522f7526a7d745e07cb63ff0ce0807cac00
Shakti Tripathi <shaktit@chetu.com>
2022-08-22 23:13:29 +0530

    minor

---
+++ shop_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\chetulearing\\shop_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=D:\\chetulearing\\shop_app"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\chetulearing\\shop_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\chetulearing\\shop_app"
+  "FLUTTER_TARGET=D:\\chetulearing\\shop_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\chetulearing\\shop_app\\.dart_tool\\package_config.json"
+)
